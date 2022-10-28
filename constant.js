727f6b2477e203b811b2a289506f761e2a62287b
srah99 <105803031+srah99@users.noreply.github.com>
2022-10-28 09:50:05 -0500

    Added
    Added 'balance' 'fund' , and the ethrs library. waiting for the contract url and the abi from the JSON file fm contract

---
+++ constant.js
@@ -0,0 +1,2 @@
+export const contractAddress = " the address to the CONTRACT HEREEE****"
+//at the part where I nee dto put the abi in here and import import the contract 
--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+import {ethers} from "./ethers-5.6.esm.min.js"
+import {contractAddress} from "./constant.js"
+
 const connectButton = document.getElementById("connectButton");
+const balanceButton = document.getElementById("balanceButton");
+const fundButton = document.getElementById("fundButton");
 connectButton.onclick = connect;
+balanceButton.onclick =getBalance;
+
 
 async function connect(){
     if (typeof window.ethereum !== "undefined"){
@@ -8,5 +15,25 @@ async function connect(){
         }catch(error){console.log(error);}
         connectButton.innerHTML="Connected";
         const accounts = await window.ethereum.request({method:"eth_accounts"});
+    }
+}
+async function getBalance(){
+    if(typeof window.ethereum !== "undefined"){
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const balance = await provider.getBalance("contractAddress");
+        console.log(balance.toString());
+
+        //provider = ethers.providers.RPCProvider("http://loaclhost:someport")
+     //const balance = await provider.getbalance(address) ;
+
+    }
+}
+async function fund(){
+    const ethAmount = document.getElementById("ethAmount").value;
+    console.log('Funding with{ethAmount}...');
+    if (typeof ethAmount !== "undefined"){
+        const provider = new ethers.providers.Web3Provider(window.etherum);
+        const sighner = provider.getSigner();
+        const contract = new ethers.Contract(contractAddress, (?? put here abi,is usaully in the json file), signer);
     }
 }
